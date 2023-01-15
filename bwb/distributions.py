8d4c170e7005de6db8d50af950e87502c99ce711
Francisco Muñoz <femunoz@dim.uchile.cl>
2023-01-15 19:31:21 -0300

    feat: integrar herramientas de OT en las clases DistributionDraw para facilitar trabajo

--- bwb/distributions.py
+++ bwb/distributions.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import abc
 from itertools import product
 from typing import List, Tuple
@@ -40,6 +42,11 @@ class DistributionDraw(Distribution):
         self._image: Image = image
         self._matrix: np.ndarray = None
         self._rng: np.random.Generator = np.random.default_rng(seed)
+        self._support: list[tuple[int, int], ...] = None
+
+        # Indices
+        self._indices: list[tuple[int, int], ...] = None
+        self._indices_inv: dict[tuple[int, int], int] = None
 
     @classmethod
     def fromarray(cls, array: np.ndarray, seed=None):
@@ -60,7 +67,7 @@ class DistributionDraw(Distribution):
 
     @property
     def matrix(self) -> np.ndarray:
-        """Matrix representation of the image."""
+        """Matrix pdf obtained from the image."""
         if self._matrix is None:
             image_as_array = np.asarray(self.image)
             if len(image_as_array.shape) == 2:
@@ -71,14 +78,73 @@ class DistributionDraw(Distribution):
             self._matrix = matrix / matrix.sum()
         return self._matrix
 
+    @property
+    def indices(self) -> list[tuple[int, int], ...]:
+        """A list of coordinates of the domain."""
+        if self._indices is None:
+            n, m = self.shape
+            self._indices = [(i, j) for i, j in product(range(n), range(m))]
+        return self._indices
+
+    @property
+    def indices_inv(self) -> dict[tuple[int, int], int]:
+        """The inverse function that return the index if the component."""
+        if self._indices_inv is None:
+            self._indices_inv: dict[tuple[int, int], int] = {val: k for k, val in enumerate(self.indices)}
+        return self._indices_inv
+
+    @property
+    def shape(self):
+        """The shape of the matrix"""
+        return self.matrix.shape
+
+    @property
+    def support(self) -> list[tuple[int, int], ...]:
+        """Support of the distribution."""
+        if self._support is None:
+            non_zero_coord: np.ndarray = np.array(np.nonzero(self.matrix)).T
+            self._support: list[tuple[int, int], ...] = [tuple(row) for row in non_zero_coord]
+        return self._support
+
+    @property
+    def weights(self):
+        """Return its correspondient weights in the same order of it support."""
+        return self.pdf(self.support)
+
     def rvs(self, size=1, random_state=None) -> list[tuple[int, int]]:
         # Set a random state
         rng = self._rng if random_state is None else np.random.default_rng(random_state)
-        n, m = self.matrix.shape
-        indices: list[tuple[int, int], ...] = [(i, j) for i, j in product(range(n), range(m))]
         # Sample with respect the weight matrix.
-        samples = rng.choice(a=len(indices), size=size, p=self.matrix.flatten())
-        return [indices[s] for s in samples]
+        samples = rng.choice(a=len(self.indices), size=size, p=self.matrix.flatten())
+        return [self.indices[s] for s in samples]
 
-    def pdf(self, *args, **kwargs):
-        return self.matrix[args]
+    def pdf(self, value: tuple[int, int] | list[tuple[int, int], ...]):
+        if isinstance(value, tuple):
+            value = [value]
+        indices_from_coord = [self.indices_inv[tup] for tup in value]
+        return self.matrix.take(indices_from_coord)
+
+    def _repr_png_(self):
+        """iPython display hook support
+
+        :returns: png version of the image as bytes
+        """
+        return self.image._repr_png_()
+
+
+class DistributionDrawBuilder:
+    """
+    Builder for the class DistributionDraw.
+    """
+
+    def __init__(self, seed=None):
+        self._rng: np.random.Generator = np.random.default_rng(seed)
+
+    def create(self, image: Image) -> DistributionDraw:
+        return DistributionDraw(image=image, seed=self._rng)
+
+    def create_fromarray(self, array: np.ndarray) -> DistributionDraw:
+        return DistributionDraw.fromarray(array=array, seed=self._rng)
+
+    def set_rng(self, rng: np.random.Generator):
+        self._rng: np.random.Generator = np.random.default_rng(rng)
