e4d163b99899927c85840681e29b367ad32ee81f
Francisco Muñoz <femunoz@dim.uchile.cl>
2023-01-24 14:59:01 -0300

    refactor: Actualizar typing

--- bwb/distributions.py
+++ bwb/distributions.py
@@ -2,14 +2,15 @@ from __future__ import annotations
 
 import abc
 import logging
+import warnings
 from collections import Counter
 from itertools import product
+from typing import TypeVar, Generic, Union, Any, Optional
 
 import PIL.Image
 import numpy as np
 from PIL.Image import Image
 from numpy.random import Generator
-
 
 # Get logger
 _log = logging.getLogger(__name__)
@@ -21,47 +22,56 @@ _formatter = logging.Formatter("%(asctime)s: %(levelname)s [%(name)s:%(lineno)s]
 # Configure a file handler
 _file_handler = logging.FileHandler(f"logging.log")
 _file_handler.setFormatter(_formatter)
-_log.addHandler(_file_handler)
+# _log.addHandler(_file_handler)
 
 _stream_handler = logging.StreamHandler()
 _stream_handler.setFormatter(_formatter)
 _log.addHandler(_stream_handler)
 
+TSample = TypeVar("TSample")
+GeneratorLike = Union[None, int, Generator]
+Coord = tuple[int, int]
 
-class RvsDistribution(abc.ABC):
+
+def default_likelihood(data: list[Coord]):
+    ...
+
+
+class RvsDistribution(abc.ABC, Generic[TSample]):
     """
     This class provides the interface for those distributions that can be sampled from them.
     """
 
-    def __init__(self, seed):
+    def __init__(self, seed: GeneratorLike):
         # Generator
         self.rng: np.random.Generator = np.random.default_rng(seed)
 
     @abc.abstractmethod
-    def rvs(self, *args, **kwargs):
+    def rvs(self, *args, **kwargs) -> list[TSample]:
         """Random variates."""
         ...
 
     @abc.abstractmethod
-    def draw(self, *args, **kwargs):
+    def draw(self, *args, **kwargs) -> TSample:
         """To draw a sample from the distribution."""
         ...
 
-    def obtain_rng(self, random_state) -> np.random.Generator:
+    def obtain_rng(self, random_state: GeneratorLike) -> Generator:
         """Obtain a random state. If the random state is `None`, returns the generator of the class"""
         return self.rng if random_state is None else np.random.default_rng(random_state)
 
 
-class Distribution(RvsDistribution, abc.ABC):
+class Distribution(RvsDistribution, abc.ABC, Generic[TSample]):
     @abc.abstractmethod
-    def pdf(self, *args, **kwargs) -> float | list[float]:
+    def pdf(self, x: TSample | list[TSample], *args, **kwargs) -> float | list[float]:
         """Probability density function."""
         ...
 
 
-class DistributionDraw(Distribution):
+class DistributionDraw(Distribution[Coord]):
+    Coord = tuple[int, int]
 
-    def __init__(self, image: Image, seed=None):
+    def __init__(self, image: Image, seed: GeneratorLike = None):
         """
         Initializer
 
@@ -78,14 +88,19 @@ class DistributionDraw(Distribution):
         # Image and pdf
         self._image: Image = image
         self._matrix: np.ndarray = None
-        self._support: list[tuple[int, int], ...] = None
+        self._support: list[Coord, ...] = None
 
         # Indices
-        self._indices: list[tuple[int, int], ...] = None
-        self._indices_inv: dict[tuple[int, int], int] = None
+        self._indices: list[Coord, ...] = None
+        self._indices_inv: dict[Coord, int] = None
 
     @classmethod
-    def fromarray(cls, array: np.ndarray, seed=None, ceil=0):
+    def fromarray(
+            cls,
+            array: np.ndarray,
+            seed: GeneratorLike = None,
+            ceil: int = 0
+    ) -> DistributionDraw[Coord]:
         """
         Alternative initializer.
 
@@ -130,12 +145,12 @@ class DistributionDraw(Distribution):
         return self._matrix
 
     @property
-    def shape(self):
+    def shape(self) -> tuple:
         """The shape of the matrix"""
         return self.matrix.shape
 
     @property
-    def indices(self) -> list[tuple[int, int], ...]:
+    def indices(self) -> list[Coord, ...]:
         """A list of coordinates of the domain."""
         if self._indices is None:
             n, m = self.shape
@@ -143,18 +158,18 @@ class DistributionDraw(Distribution):
         return self._indices
 
     @property
-    def indices_inv(self) -> dict[tuple[int, int], int]:
+    def indices_inv(self) -> dict[Coord, int]:
         """The inverse function that return the index if the component."""
         if self._indices_inv is None:
-            self._indices_inv: dict[tuple[int, int], int] = {val: k for k, val in enumerate(self.indices)}
+            self._indices_inv: dict[Coord, int] = {val: k for k, val in enumerate(self.indices)}
         return self._indices_inv
 
     @property
-    def support(self) -> list[tuple[int, int], ...]:
+    def support(self) -> list[Coord, ...]:
         """Support of the distribution."""
         if self._support is None:
             non_zero_coord: np.ndarray = np.array(np.nonzero(self.matrix)).T
-            self._support: list[tuple[int, int], ...] = [tuple(row) for row in non_zero_coord]
+            self._support: list[Coord, ...] = [tuple(row) for row in non_zero_coord]
         return self._support
 
     @property
@@ -162,20 +177,20 @@ class DistributionDraw(Distribution):
         """Return its correspondient weights in the same order of it support."""
         return self.pdf(self.support)
 
-    def rvs(self, size=1, random_state=None) -> list[tuple[int, int]]:
+    def rvs(self, size: int = 1, random_state: GeneratorLike = None) -> list[Coord]:
         # Set a random state
         rng = self.obtain_rng(random_state)
         # Sample with respect the weight matrix.
         samples = rng.choice(a=len(self.indices), size=size, p=self.matrix.flatten())
         return [self.indices[s] for s in samples]
 
-    def draw(self, random_state=None) -> tuple[int, int]:
+    def draw(self, random_state: GeneratorLike = None) -> Coord:
         return self.rvs(size=1, random_state=random_state)[0]
 
-    def pdf(self, value: tuple[int, int] | list[tuple[int, int], ...]) -> float | list[float]:
-        if isinstance(value, tuple):
-            return self.matrix[value]
-        indices_from_coord = [self.indices_inv[tup] for tup in value]
+    def pdf(self, x: Coord | list[Coord], **kwargs) -> float | list[float]:
+        if isinstance(x, tuple):
+            return self.matrix[x]
+        indices_from_coord = [self.indices_inv[tup] for tup in x]
         return self.matrix.take(indices_from_coord)
 
 
@@ -184,11 +199,11 @@ class DistributionDrawBuilder:
     Builder for the class DistributionDraw.
     """
 
-    def __init__(self, seed=None, ceil=0):
-        self._rng: np.random.Generator = np.random.default_rng(seed)
-        self._ceil = ceil
+    def __init__(self, seed: GeneratorLike = None, ceil: int = 0):
+        self._rng: Generator = np.random.default_rng(seed)
+        self._ceil: int = ceil
 
-        self._dict_kwargs = dict(seed=self._rng, ceil=self._ceil)
+        self._dict_kwargs: dict[str, Any] = dict(seed=self._rng, ceil=self._ceil)
 
     def create(self, image: Image) -> DistributionDraw:
         return DistributionDraw(image=image, **self._dict_kwargs)
@@ -196,8 +211,8 @@ class DistributionDrawBuilder:
     def create_fromarray(self, array: np.ndarray) -> DistributionDraw:
         return DistributionDraw.fromarray(array=array, **self._dict_kwargs)
 
-    def set_rng(self, rng: np.random.Generator):
-        self._rng: np.random.Generator = np.random.default_rng(rng)
+    def set_rng(self, rng: GeneratorLike):
+        self._rng = np.random.default_rng(rng)
         self._dict_kwargs["seed"] = self._rng
         return self
 
@@ -207,14 +222,14 @@ class DistributionDrawBuilder:
         return self
 
 
-class PosteriorPiN(RvsDistribution, abc.ABC):
+class PosteriorPiN(RvsDistribution[TSample], abc.ABC):
     """Abstract class used as protocol for the distributions used as Posterior."""
 
     def __init__(
             self,
-            data: list[tuple[int, int], ...],
-            models: list[DistributionDraw],
-            seed,
+            data: list[TSample, ...],
+            models: list[Distribution],
+            seed: GeneratorLike,
     ):
         super().__init__(seed)
 
@@ -222,9 +237,9 @@ class PosteriorPiN(RvsDistribution, abc.ABC):
         self._i = 0
 
         # Data = {x_i}_{i=1}^n
-        self.data: list[tuple[int, int], ...] = data
+        self.data: list[TSample] = data
         # Models space M = {m_i}_{i=1}^M
-        self.models: list[DistributionDraw] = models
+        self.models: list[Distribution] = models
         # Likelihood cache
         self._likelihood_cache: np.ndarray = None
         self.counter: Counter = Counter()
@@ -240,19 +255,22 @@ class PosteriorPiN(RvsDistribution, abc.ABC):
         return len(self.models)
 
     @property
-    def likelihood_cache(self) -> np.ndarray:
+    def likelihood_cache(self) -> np.ndarray[float]:
         """Likelihood cache to avoid to compute every time a same distribution, in order to be the same order of
         the list `models`."""
         if self._likelihood_cache is None or len(self._likelihood_cache) != self.n_models:
             _log.debug(f"Calculating the likelihood cache...")
             self._likelihood_cache = np.array([self.likelihood(m) for m in self.models])
             _log.debug(f"Likelihood cache calculated.")
+
+            if sum(self._likelihood_cache > 0) == 1:
+                warnings.warn("The likelihood support has only one element.")
         return self._likelihood_cache
 
     def __repr__(self):
         return self.__class__.__name__ + f"(n_data={self.n_data}, n_models={self.n_models})"
 
-    def likelihood(self, m: DistributionDraw) -> float:
+    def likelihood(self, m: Distribution) -> float:
         """
         Likelihood of the function:
 
@@ -260,7 +278,7 @@ class PosteriorPiN(RvsDistribution, abc.ABC):
 
         Parameters
         ----------
-        m: DistributionDraw
+        m: Distribution
             The distribution to calculate the likelihood.
 
         Returns
@@ -279,12 +297,12 @@ class PosteriorPiN(RvsDistribution, abc.ABC):
         # Return the product of evaluations
         return evaluations_prod
 
-    def rvs(self, size=1, random_state=None) -> list[DistributionDraw]:
+    def rvs(self, size: int = 1, random_state: GeneratorLike = None) -> list[Distribution]:
         # Set a random state
         rng = self.obtain_rng(random_state)
         return [self.draw(random_state=rng) for _ in range(size)]
 
-    def draw(self, random_state=None) -> DistributionDraw:
+    def draw(self, random_state: GeneratorLike = None) -> Distribution:
         _log.debug("=" * 10 + f" i = {self._i} " + "=" * 10)
         self._i += 1
         draw, i = self._draw(random_state=random_state)
@@ -292,49 +310,52 @@ class PosteriorPiN(RvsDistribution, abc.ABC):
         return draw
 
     @abc.abstractmethod
-    def _draw(self, random_state) -> tuple[DistributionDraw, int]:
+    def _draw(self, random_state: GeneratorLike) -> tuple[Distribution, int]:
         """To use template pattern. Returns a distribution and its index."""
         ...
 
-    def most_common(self, n=None) -> list[DistributionDraw]:
+    def most_common(self, n: Optional[int] = None) -> list[Distribution]:
         return [self.models[k] for k, _ in self.counter.most_common(n)]
 
 
-class MCMCPosteriorPiN(PosteriorPiN, abc.ABC):
+class MCMCPosteriorPiN(PosteriorPiN[TSample], abc.ABC):
     """Abstract class for the implementations of the posterior, using MCMC algorithms."""
 
     def __init__(
             self,
-            data: list[tuple[int, int], ...],
-            models: list[DistributionDraw],
-            seed,
-            lazy_init,
+            data: list[TSample, ...],
+            models: list[Distribution],
+            seed: GeneratorLike,
+            lazy_init: bool,
     ):
         super().__init__(data, models, seed)
         # History {mu^(i)}_{i=1}^N
-        self.history: list[DistributionDraw] = []
+        self.history: list[Distribution] = []
         self.last_i: int = None
         if not lazy_init:
             self._first_step(self.rng)
 
     def __repr__(self):
-        return (self.__class__.__name__ + f"(n_data={self.n_data}, n_models={self.n_models}, "
-                f"n_samples={len(self.history) - 1}, last_i={self.last_i})")
+        return (self.__class__.__name__
+                + f"(n_data={self.n_data}, n_models={self.n_models}, "
+                  f"n_samples={len(self.history) - 1}, last_i={self.last_i})")
 
     @abc.abstractmethod
-    def _first_step(self, rng):
+    def _first_step(self, rng: GeneratorLike):
         ...
 
 
-class MetropolisPosteriorPiN(MCMCPosteriorPiN):
+class MetropolisPosteriorPiN(MCMCPosteriorPiN[TSample]):
 
     def __init__(
             self,
-            data: list[tuple[int, int], ...], models: list[DistributionDraw],
-            seed=None, lazy_init=False
+            data: list[TSample, ...],
+            models: list[Distribution],
+            seed: GeneratorLike = None,
+            lazy_init: bool = False,
     ):
         super().__init__(data, models, seed, lazy_init)
-        self.possible_models = np.arange(self.n_models)
+        self.possible_models: np.ndarray[int] = np.arange(self.n_models)
 
     def _first_step(self, rng):
         _log.info("Executing _first_step")
@@ -348,7 +369,7 @@ class MetropolisPosteriorPiN(MCMCPosteriorPiN):
         # Update the counter
         self.counter[self.last_i] += 1
 
-    def _draw(self, random_state=None) -> tuple[DistributionDraw, int]:
+    def _draw(self, random_state: GeneratorLike = None) -> tuple[Distribution, int]:
         # Set generator
         rng = self.obtain_rng(random_state)
 
@@ -384,7 +405,7 @@ class MetropolisPosteriorPiN(MCMCPosteriorPiN):
 
         # Acceptance / Rejection
         if u < A_mu_i_mu_star:
-            _log.debug(f"{last_i = } -> {next_i = }.")
+            _log.info(f"i = {self._i}: {last_i = } -> {next_i = }.")
             # Add to the history
             mu_star = self.models[next_i]
             self.history.append(mu_star)
@@ -400,11 +421,13 @@ class MetropolisPosteriorPiN(MCMCPosteriorPiN):
         return self.history[-1], last_i
 
 
-class AlternativeMetropolisPosteriorPiN(MetropolisPosteriorPiN):
+class AlternativeMetropolisPosteriorPiN(MetropolisPosteriorPiN[TSample]):
     def __init__(
             self,
-            data: list[tuple[int, int], ...], models: list[DistributionDraw],
-            seed=None, lazy_init=False
+            data: list[TSample],
+            models: list[Distribution],
+            seed: GeneratorLike = None,
+            lazy_init: bool = False,
     ):
         super().__init__(data, models, seed, True)
         self.models = [m for m in self.models if self.likelihood(m) > 0]
@@ -414,14 +437,14 @@ class AlternativeMetropolisPosteriorPiN(MetropolisPosteriorPiN):
             self._first_step(self.rng)
 
 
-class GibbsPosteriorPiN(MCMCPosteriorPiN):
+class GibbsPosteriorPiN(MCMCPosteriorPiN[TSample]):
 
     def __init__(
             self,
-            data: list[tuple[int, int], ...],
-            models: list[DistributionDraw],
-            seed=None,
-            lazy_init=False,
+            data: list[TSample],
+            models: list[Distribution],
+            seed: GeneratorLike = None,
+            lazy_init: bool = False,
     ):
         super().__init__(data, models, seed, True)
         self.possible_models = np.arange(self.n_models)
@@ -432,8 +455,10 @@ class GibbsPosteriorPiN(MCMCPosteriorPiN):
             self._first_step(self.rng)
 
     def __repr__(self):
-        return (self.__class__.__name__ + f"(n_data={self.n_data}, n_models={self.n_models}, "
-                f"n_samples={len(self.history) - 1}, last_i={self.last_i}, likelihood_sum={self.likelihood_sum})")
+        return (self.__class__.__name__
+                + f"(n_data={self.n_data}, n_models={self.n_models}, "
+                  f"n_samples={len(self.history) - 1}, last_i={self.last_i},"
+                  f" likelihood_sum={self.likelihood_sum:.4e})")
 
     def _first_step(self, rng):
         _log.info("Executing _first_step")
@@ -442,12 +467,12 @@ class GibbsPosteriorPiN(MCMCPosteriorPiN):
         # Choose a model
         possible_distributions = np.arange(self.n_models)[self.likelihood_cache > 0]
         self.last_i = int(rng.choice(possible_distributions))
-        _log.info(f"First model selected: {self.last_i}.")
         self.history.append(self.models[self.last_i])
+        _log.info(f"First model selected: {self.last_i}.")
         # Update the counter
         self.counter[self.last_i] += 1
 
-    def _draw(self, random_state=None) -> tuple[DistributionDraw, int]:
+    def _draw(self, random_state=None) -> tuple[Distribution, int]:
         # Set generator
         rng = self.obtain_rng(random_state)
 
@@ -481,11 +506,10 @@ class GibbsPosteriorPiN(MCMCPosteriorPiN):
             / (self.likelihood_sum - self.likelihood_cache[next_i])
         )
         _log.debug(f"{A_mu_i_mu_star = }")
-        _log.debug(f"{self.likelihood_cache[next_i] = }")
 
         # Acceptance / Rejection
         if u < A_mu_i_mu_star:
-            _log.debug(f"{last_i = } -> {next_i = }.")
+            _log.info(f"i = {self._i}: {last_i = } -> {next_i = }.")
             # Add to the history
             mu_star = self.models[next_i]
             self.history.append(mu_star)
@@ -500,11 +524,11 @@ class GibbsPosteriorPiN(MCMCPosteriorPiN):
         return self.history[-1], last_i
 
 
-class AlternativeGibbsPosteriorPiN(GibbsPosteriorPiN):
+class AlternativeGibbsPosteriorPiN(GibbsPosteriorPiN[TSample]):
     def __init__(
             self,
-            data: list[tuple[int, int], ...],
-            models: list[DistributionDraw],
+            data: list[TSample],
+            models: list[Distribution],
             seed=None,
             lazy_init=False,
     ):
@@ -515,4 +539,3 @@ class AlternativeGibbsPosteriorPiN(GibbsPosteriorPiN):
 
         if not lazy_init:
             self._first_step(self.rng)
-
