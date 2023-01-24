f88ad6fe0f49ed5851d4fca4fecdf7719e9e10c2
Francisco Muñoz <femunoz@dim.uchile.cl>
2023-01-24 13:21:58 -0300

    feat: Agregar logger y otros refactors

--- bwb/distributions.py
+++ bwb/distributions.py
@@ -1,11 +1,31 @@
 from __future__ import annotations
 
 import abc
+import logging
+from collections import Counter
 from itertools import product
 
 import PIL.Image
 import numpy as np
 from PIL.Image import Image
+from numpy.random import Generator
+
+
+# Get logger
+_log = logging.getLogger(__name__)
+_log.setLevel(logging.WARNING)
+
+# Configure a formatter
+_formatter = logging.Formatter("%(asctime)s: %(levelname)s [%(name)s:%(lineno)s] %(message)s")
+
+# Configure a file handler
+_file_handler = logging.FileHandler(f"logging.log")
+_file_handler.setFormatter(_formatter)
+_log.addHandler(_file_handler)
+
+_stream_handler = logging.StreamHandler()
+_stream_handler.setFormatter(_formatter)
+_log.addHandler(_stream_handler)
 
 
 class RvsDistribution(abc.ABC):
@@ -27,12 +47,19 @@ class RvsDistribution(abc.ABC):
         """To draw a sample from the distribution."""
         ...
 
-    def obtain_rng(self, random_state):
+    def obtain_rng(self, random_state) -> np.random.Generator:
         """Obtain a random state. If the random state is `None`, returns the generator of the class"""
         return self.rng if random_state is None else np.random.default_rng(random_state)
 
 
-class DistributionDraw(RvsDistribution):
+class Distribution(RvsDistribution, abc.ABC):
+    @abc.abstractmethod
+    def pdf(self, *args, **kwargs) -> float | list[float]:
+        """Probability density function."""
+        ...
+
+
+class DistributionDraw(Distribution):
 
     def __init__(self, image: Image, seed=None):
         """
@@ -145,10 +172,9 @@ class DistributionDraw(RvsDistribution):
     def draw(self, random_state=None) -> tuple[int, int]:
         return self.rvs(size=1, random_state=random_state)[0]
 
-    def pdf(self, value: tuple[int, int] | list[tuple[int, int], ...]):
-        """Probability density function."""
+    def pdf(self, value: tuple[int, int] | list[tuple[int, int], ...]) -> float | list[float]:
         if isinstance(value, tuple):
-            value = [value]
+            return self.matrix[value]
         indices_from_coord = [self.indices_inv[tup] for tup in value]
         return self.matrix.take(indices_from_coord)
 
@@ -192,14 +218,39 @@ class PosteriorPiN(RvsDistribution, abc.ABC):
     ):
         super().__init__(seed)
 
+        # Number of the iteration
+        self._i = 0
+
         # Data = {x_i}_{i=1}^n
         self.data: list[tuple[int, int], ...] = data
-        self.n_data: int = len(self.data)
         # Models space M = {m_i}_{i=1}^M
         self.models: list[DistributionDraw] = models
-        self.n_models = len(self.models)
         # Likelihood cache
         self._likelihood_cache: np.ndarray = None
+        self.counter: Counter = Counter()
+
+        _log.info(f"init PosteriorPiN: n_data={self.n_data}, n_models={self.n_models}")
+
+    @property
+    def n_data(self) -> int:
+        return len(self.data)
+
+    @property
+    def n_models(self) -> int:
+        return len(self.models)
+
+    @property
+    def likelihood_cache(self) -> np.ndarray:
+        """Likelihood cache to avoid to compute every time a same distribution, in order to be the same order of
+        the list `models`."""
+        if self._likelihood_cache is None or len(self._likelihood_cache) != self.n_models:
+            _log.debug(f"Calculating the likelihood cache...")
+            self._likelihood_cache = np.array([self.likelihood(m) for m in self.models])
+            _log.debug(f"Likelihood cache calculated.")
+        return self._likelihood_cache
+
+    def __repr__(self):
+        return self.__class__.__name__ + f"(n_data={self.n_data}, n_models={self.n_models})"
 
     def likelihood(self, m: DistributionDraw) -> float:
         """
@@ -217,26 +268,16 @@ class PosteriorPiN(RvsDistribution, abc.ABC):
         float
             The likelihood computed.
         """
-        # List of evaluations
-        evaluations = []
+        evaluations_prod = 1.
         # Iterate over every value of the data
         for i in range(self.n_data):
-            evaluation = m.matrix[self.data[i]]
+            evaluations_prod *= m.pdf(self.data[i])
             # If the evaluation is zero, return 0 directly
-            if evaluation == 0:
+            if evaluations_prod == 0:
                 return 0.
-            evaluations.append(evaluation)
 
         # Return the product of evaluations
-        return np.prod(evaluations)
-
-    @property
-    def likelihood_cache(self) -> np.ndarray:
-        """Likelihood cache to avoid to compute every time a same distribution, in order to be the same order of
-        the list `models`."""
-        if not self._likelihood_cache:
-            self._likelihood_cache = np.array([self.likelihood(m) for m in self.models])
-        return self._likelihood_cache
+        return evaluations_prod
 
     def rvs(self, size=1, random_state=None) -> list[DistributionDraw]:
         # Set a random state
@@ -244,7 +285,19 @@ class PosteriorPiN(RvsDistribution, abc.ABC):
         return [self.draw(random_state=rng) for _ in range(size)]
 
     def draw(self, random_state=None) -> DistributionDraw:
+        _log.debug("=" * 10 + f" i = {self._i} " + "=" * 10)
+        self._i += 1
+        draw, i = self._draw(random_state=random_state)
+        self.counter[i] += 1
+        return draw
+
+    @abc.abstractmethod
+    def _draw(self, random_state) -> tuple[DistributionDraw, int]:
+        """To use template pattern. Returns a distribution and its index."""
         ...
+
+    def most_common(self, n=None) -> list[DistributionDraw]:
+        return [self.models[k] for k, _ in self.counter.most_common(n)]
 
 
 class MCMCPosteriorPiN(PosteriorPiN, abc.ABC):
@@ -261,13 +314,12 @@ class MCMCPosteriorPiN(PosteriorPiN, abc.ABC):
         # History {mu^(i)}_{i=1}^N
         self.history: list[DistributionDraw] = []
         self.last_i: int = None
-        self.counter: dict[int, int] = {}
         if not lazy_init:
             self._first_step(self.rng)
 
-    def update_counter(self, i):
-        self.counter.setdefault(i, 0)
-        self.counter[i] += 1
+    def __repr__(self):
+        return (self.__class__.__name__ + f"(n_data={self.n_data}, n_models={self.n_models}, "
+                f"n_samples={len(self.history) - 1}, last_i={self.last_i})")
 
     @abc.abstractmethod
     def _first_step(self, rng):
@@ -282,33 +334,44 @@ class MetropolisPosteriorPiN(MCMCPosteriorPiN):
             seed=None, lazy_init=False
     ):
         super().__init__(data, models, seed, lazy_init)
+        self.possible_models = np.arange(self.n_models)
 
     def _first_step(self, rng):
-        if not self.history:
-            # To avoid a measure with zero likelihood
-            probabilities = (self.likelihood_cache > 0).astype(float)
-            probabilities /= probabilities.sum()
-            # Choose a model
-            self.last_i = int(rng.choice(self.n_models, p=probabilities))
-            self.history.append(self.models[self.last_i])
-            # Update the counter
-            self.update_counter(self.last_i)
+        _log.info("Executing _first_step")
+        # To avoid a measure with zero likelihood
+        probabilities = (self.likelihood_cache > 0).astype(float)
+        probabilities /= probabilities.sum()
+        # Choose a model
+        self.last_i = int(rng.choice(self.n_models, p=probabilities))
+        _log.info(f"First model selected: {self.last_i}.")
+        self.history.append(self.models[self.last_i])
+        # Update the counter
+        self.counter[self.last_i] += 1
 
-    def draw(self, random_state=None):
+    def _draw(self, random_state=None) -> tuple[DistributionDraw, int]:
         # Set generator
         rng = self.obtain_rng(random_state)
 
         # If this is the first time that executes this instruction
-        self._first_step(rng)
+        if len(self.history) == 0:
+            self._first_step(rng)
         last_i = self.last_i
+
+        if sum(self.likelihood_cache > 0) == 1:
+            mu_i = self.history[-1]
+            self.history.append(mu_i)
+            return self.history[-1], self.last_i
 
         # Draw a uniform
         u = rng.uniform(low=0, high=1)
+        _log.debug(f"{u = }")
 
         # Draw a candidate
-        possible_faces = list(range(self.n_models))
-        possible_faces.remove(last_i)  # Remove the last face
-        next_i = int(rng.choice(possible_faces))
+        probabilities = np.ones(self.n_models)
+        probabilities[last_i] = 0.  # Remove the last distribution
+        probabilities /= probabilities.sum()
+
+        next_i = int(rng.choice(self.possible_models, p=probabilities))
 
         # Compute the acceptance probability
         A_mu_i_mu_star = min(
@@ -316,9 +379,12 @@ class MetropolisPosteriorPiN(MCMCPosteriorPiN):
             (self.likelihood_cache[next_i])
             / (self.likelihood_cache[last_i])
         )
+        _log.debug(f"{A_mu_i_mu_star = }")
+        _log.debug(f"{self.likelihood_cache[next_i] = }")
 
         # Acceptance / Rejection
         if u < A_mu_i_mu_star:
+            _log.debug(f"{last_i = } -> {next_i = }.")
             # Add to the history
             mu_star = self.models[next_i]
             self.history.append(mu_star)
@@ -331,10 +397,21 @@ class MetropolisPosteriorPiN(MCMCPosteriorPiN):
             mu_i = self.history[-1]
             self.history.append(mu_i)
 
-        # Update the counter
-        self.update_counter(self.last_i)
+        return self.history[-1], last_i
 
-        return self.history[-1]
+
+class AlternativeMetropolisPosteriorPiN(MetropolisPosteriorPiN):
+    def __init__(
+            self,
+            data: list[tuple[int, int], ...], models: list[DistributionDraw],
+            seed=None, lazy_init=False
+    ):
+        super().__init__(data, models, seed, True)
+        self.models = [m for m in self.models if self.likelihood(m) > 0]
+        self.possible_models = np.arange(self.n_models)
+
+        if not lazy_init:
+            self._first_step(self.rng)
 
 
 class GibbsPosteriorPiN(MCMCPosteriorPiN):
@@ -346,39 +423,54 @@ class GibbsPosteriorPiN(MCMCPosteriorPiN):
             seed=None,
             lazy_init=False,
     ):
-        super().__init__(data, models, seed, lazy_init)
-        self.possible_models = list(range(self.n_models))
-        self.likelihood_sum: float = 0.
+        super().__init__(data, models, seed, True)
+        self.possible_models = np.arange(self.n_models)
         self.probs_cache: dict[int, np.ndarray] = dict()
+        self.likelihood_sum: float = 0.
+
+        if not lazy_init:
+            self._first_step(self.rng)
+
+    def __repr__(self):
+        return (self.__class__.__name__ + f"(n_data={self.n_data}, n_models={self.n_models}, "
+                f"n_samples={len(self.history) - 1}, last_i={self.last_i}, likelihood_sum={self.likelihood_sum})")
 
     def _first_step(self, rng):
-        if not self.history:
-            self.likelihood_sum = sum(self.likelihood_cache)
-            # Choose a model
-            self.last_i = int(rng.choice(self.n_models))
-            self.history.append(self.models[self.last_i])
-            # Update the counter
-            self.update_counter(self.last_i)
+        _log.info("Executing _first_step")
+        self.likelihood_sum = np.sum(self.likelihood_cache)
+        _log.debug(f"likelihood_sum = {self.likelihood_sum}")
+        # Choose a model
+        possible_distributions = np.arange(self.n_models)[self.likelihood_cache > 0]
+        self.last_i = int(rng.choice(possible_distributions))
+        _log.info(f"First model selected: {self.last_i}.")
+        self.history.append(self.models[self.last_i])
+        # Update the counter
+        self.counter[self.last_i] += 1
 
-    def draw(self, random_state=None):
+    def _draw(self, random_state=None) -> tuple[DistributionDraw, int]:
         # Set generator
         rng = self.obtain_rng(random_state)
 
         # If this is the first time that executes this instruction
-        self._first_step(rng)
+        if len(self.history) == 0:
+            self._first_step(rng)
         last_i = self.last_i
+
+        if sum(self.likelihood_cache > 0) == 1:
+            mu_i = self.history[-1]
+            self.history.append(mu_i)
+            return self.history[-1], self.last_i
 
         # Draw a uniform
         u = rng.uniform(low=0, high=1)
+        _log.debug(f"{u = }")
 
         # Draw a candidate
         if last_i not in self.probs_cache:
-            self.probs_cache[last_i] = np.array(
-                self.likelihood_cache[i]
-                / (self.likelihood_sum - self.likelihood_cache[last_i])
-                for i in self.possible_models
-            )
+            _log.debug(f"Computing the cache probabilities for the draw {last_i}.")
+            self.probs_cache[last_i] = self.likelihood_cache.copy()
             self.probs_cache[last_i][last_i] = 0.
+            self.probs_cache[last_i] /= self.probs_cache[last_i].sum()
 
         next_i = int(rng.choice(self.possible_models, p=self.probs_cache[last_i]))
 
@@ -388,9 +480,12 @@ class GibbsPosteriorPiN(MCMCPosteriorPiN):
             (self.likelihood_sum - self.likelihood_cache[last_i])
             / (self.likelihood_sum - self.likelihood_cache[next_i])
         )
+        _log.debug(f"{A_mu_i_mu_star = }")
+        _log.debug(f"{self.likelihood_cache[next_i] = }")
 
         # Acceptance / Rejection
         if u < A_mu_i_mu_star:
+            _log.debug(f"{last_i = } -> {next_i = }.")
             # Add to the history
             mu_star = self.models[next_i]
             self.history.append(mu_star)
@@ -402,7 +497,22 @@ class GibbsPosteriorPiN(MCMCPosteriorPiN):
             mu_i = self.history[-1]
             self.history.append(mu_i)
 
-        # Update the counter
-        self.update_counter(last_i)
+        return self.history[-1], last_i
 
-        return self.history[-1]
+
+class AlternativeGibbsPosteriorPiN(GibbsPosteriorPiN):
+    def __init__(
+            self,
+            data: list[tuple[int, int], ...],
+            models: list[DistributionDraw],
+            seed=None,
+            lazy_init=False,
+    ):
+        super().__init__(data, models, seed, True)
+
+        self.models = [m for m in self.models if self.likelihood(m) > 0]
+        self.possible_models = np.arange(self.n_models)
+
+        if not lazy_init:
+            self._first_step(self.rng)
+
