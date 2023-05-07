7ddeb81a6c2b204c636eebb3e4e754bf2cb4422e
Francisco Muñoz <femunoz@dim.uchile.cl>
2023-05-06 21:53:59 -0400

    refactor: Minor structural changes

--- bwb/distributions/posterior_distributions.py
+++ bwb/distributions/posterior_distributions.py
@@ -1,17 +1,17 @@
 import abc
+import collections
 import functools
 import time
-from collections import Counter
-from typing import Sequence
+import typing
 
 import numpy as np
 import torch
-from numpy.random import Generator
 
-from bwb.config import Config
-from bwb.distributions.data_loaders import BaseDistributionDataLoader
-from bwb.distributions.discrete_distribution import DiscreteDistribution
-from bwb.validation import check_is_fitted
+import bwb.distributions as distrib
+import bwb.distributions.data_loaders as data_loaders
+import bwb.validation as validation
+from bwb.config import config
+from bwb.utils import _ArrayLike, _DistributionT
 
 __all__ = [
     "PosteriorPiN",
@@ -19,7 +19,7 @@ __all__ = [
 ]
 
 
-def _log_likelihood_default(model: DiscreteDistribution, data: torch.Tensor):
+def _log_likelihood_default(model: distrib.DiscreteDistribution, data: torch.Tensor):
     """ Default log-likelihood of the posterior.
 
     :param model: A model to obtain its log-likelihood
@@ -29,9 +29,9 @@ def _log_likelihood_default(model: DiscreteDistribution, data: torch.Tensor):
     return torch.sum(model.log_prob(data))
 
 
-def timeit_to_total_time(method):
-    """Function that records the total time it takes to execute a method, and stores it in the ``total_time``
-    attribute of the class instance."""
+def _timeit_to_total_time(method):
+    """Function that records the total time it takes to execute a method, and stores it in the
+    ``total_time`` attribute of the class instance."""
 
     @functools.wraps(method)
     def timeit_wrapper(*args, **kwargs):
@@ -45,11 +45,12 @@ def timeit_to_total_time(method):
 
 
 # noinspection PyAttributeOutsideInit
-class PosteriorPiN(abc.ABC):
+class PosteriorPiN(abc.ABC, typing.Generic[_DistributionT]):
     r"""Base class for classes representing the posterior distribution:
 
     .. math::
-        \Pi_n(dm) = \Pi(dm) \frac{\mathcal{L}_n(m)}{\int_{\mathcal{M}} \mathcal{L}_n(d\bar m) \Pi(d\bar m)}
+        \Pi_n(dm)
+        = \Pi(dm) \frac{\mathcal{L}_n(m)}{\int_{\mathcal{M}} \mathcal{L}_n(d\bar m) \Pi(d\bar m)}
 
     """
 
@@ -58,24 +59,26 @@ class PosteriorPiN(abc.ABC):
             log_likelihood_fn,
     ):
         """
-        Initiliser.
+        Initialiser.
 
-        :param log_likelihood_fn: The log-likelihood function. Is a function that receives a model and data, and
-        returns the log-likelihood.
+        :param log_likelihood_fn: The log-likelihood function. Is a function that receives a model
+        and data, and returns the log-likelihood.
         """
         self.log_likelihood_fn = log_likelihood_fn or _log_likelihood_default
 
         # The history of the index samples
         self.samples_history: list[int] = []
-        self.samples_counter: Counter = Counter()
+        self.samples_counter: collections.Counter = collections.Counter()
 
         # To register the total time of the samples
         self.total_time = 0.
 
+        self.__fitted = False
+
     def fit(
             self,
-            data: Sequence[int],
-            models: BaseDistributionDataLoader,
+            data: _ArrayLike,
+            models: data_loaders.BaseDistributionDataLoader[_DistributionT],
     ):
         """
         Fit the data of the distribution
@@ -84,17 +87,18 @@ class PosteriorPiN(abc.ABC):
         :param models: A sequence of models.
         :return: itself.
         """
-        self.data_ = torch.as_tensor(data, device=Config.device)
-        self.models_: BaseDistributionDataLoader = models
+        self.data_ = torch.as_tensor(data, device=config.device)
+        self.models_: data_loaders.BaseDistributionDataLoader[_DistributionT] = models
         self.models_index_ = np.arange(len(self.models_))
+        self.__fitted = True
         return self
 
-    def log_likelihood(self, model: DiscreteDistribution):
+    def log_likelihood(self, model: _DistributionT):
         """The log-likelihood of the model."""
-        check_is_fitted(self)
+        validation.check_is_fitted(self)
         return self.log_likelihood_fn(model, data=self.data_)
 
-    def likelihood(self, model: DiscreteDistribution):
+    def likelihood(self, model: _DistributionT):
         """The likelihood of the model."""
         return torch.exp(self.log_likelihood(model))
 
@@ -108,35 +112,45 @@ class PosteriorPiN(abc.ABC):
         """To use template pattern on ``_rvs`` method."""
         ...
 
-    @timeit_to_total_time
-    def draw(self, *args, **kwargs) -> DiscreteDistribution:
+    @_timeit_to_total_time
+    def draw(self, *args, **kwargs) -> _DistributionT:
         """Draw a sample."""
-        check_is_fitted(self)
+        validation.check_is_fitted(self)
         to_return, i = self._draw(*args, **kwargs)
         self.samples_history.append(i)
         self.samples_counter[i] += 1
         return to_return
 
-    @timeit_to_total_time
-    def rvs(self, size=1, *args, **kwargs) -> Sequence[DiscreteDistribution]:
+    @_timeit_to_total_time
+    def rvs(self, size=1, *args, **kwargs) -> typing.Sequence[_DistributionT]:
         """Samples as many distributions as the ``size`` parameter indicates."""
-        check_is_fitted(self)
+        validation.check_is_fitted(self)
         to_return, list_i = self._rvs(size=size, *args, **kwargs)
         self.samples_history += list_i
         self.samples_counter.update(list_i)
         return to_return
 
+    def __repr__(self):
+        if self.__fitted:
+            return (self.__class__.__name__
+                    + "("
+                    + f"n_data={len(self.data_)}, "
+                    + f"n_models={len(self.models_)}"
+                    + ")")
+        return self.__class__.__name__ + "()"
+
 
 # noinspection PyAttributeOutsideInit
-class ExplicitPosteriorPiN(PosteriorPiN):
-    r"""Distribution that uses the strategy of calculating all likelihoods by brute force. This class implements
-    likelihoods of the form
+class ExplicitPosteriorPiN(PosteriorPiN[_DistributionT]):
+    r"""Distribution that uses the strategy of calculating all likelihoods by brute force. This
+    class implements likelihoods of the form
 
     .. math::
         \mathcal{L}_n(m) = \prod_{i=1}^{n} \rho_{m}(x_i)
 
-    using the log-likelihood for stability. Finally, to compute the sampling probabilities, for a discrete
-    set :math:`\mathcal{M}` of models, using a uniform prior, we have the posterior explicit by
+    using the log-likelihood for stability. Finally, to compute the sampling probabilities, for a
+    discrete set :math:`\mathcal{M}` of models, using a uniform prior, we have the posterior
+    explicit by
 
     .. math::
         \Pi_n(m) = \frac{\mathcal{L}_n(m)}{\sum_{\bar m \in \mathcal{M}} \mathcal{L}_n(\bar m)}
@@ -151,8 +165,8 @@ class ExplicitPosteriorPiN(PosteriorPiN):
 
     def fit(
             self,
-            data: Sequence[int],
-            models: BaseDistributionDataLoader,
+            data,
+            models,
     ):
         super(ExplicitPosteriorPiN, self).fit(data, models)
 
@@ -174,18 +188,12 @@ class ExplicitPosteriorPiN(PosteriorPiN):
 
         return self
 
-    def _draw(self, seed=None):
-        rng: Generator = np.random.default_rng(seed)
+    def _draw(self, seed=None) -> _DistributionT:
+        rng: np.random.Generator = np.random.default_rng(seed)
         i = rng.choice(a=self.models_index_, p=self.probabilities_)
         return self.models_[i], i
 
-    def _rvs(self, size=1, seed=None, **kwargs):
-        rng: Generator = np.random.default_rng(seed)
+    def _rvs(self, size=1, seed=None, **kwargs) -> typing.Sequence[_DistributionT]:
+        rng: np.random.Generator = np.random.default_rng(seed)
         list_i = list(rng.choice(a=self.models_index_, size=size, p=self.probabilities_))
         return [self.models_[i] for i in list_i], list_i
-
-    def draw(self, seed=None, **kwargs):
-        super().draw(seed=seed)
-
-    def rvs(self, size=1, seed=None, **kwargs):
-        super().rvs(size=size, seed=seed)
