07f52b7de00d4d79a1e562d4c33e2f1009a78c0a
Francisco Muñoz <fmunoz@ug.uchile.cl>
2024-04-23 16:13:09 -0400

    feat: add methods at the config singleton, refactor of documentation and add exceptions

--- bwb/config.py
+++ bwb/config.py
@@ -1,8 +1,9 @@
-"""
-Configuration module for the library. This module contains the configuration class and some helper functions to
-change the configuration. The configuration is a singleton, so it is shared across the whole library.
-"""
+"""Configuration module for the library. This module contains the configuration class and some
+helper functions to change the configuration. The configuration is a singleton, so it is shared
+across the whole library."""
 import threading
+import typing as t
+import warnings
 
 import torch
 
@@ -27,9 +28,23 @@ class _SingletonMeta(type):
         return cls._instances[cls]
 
 
+class CudaNotAvailableError(Exception):
+    """
+    Exception raised when CUDA is not available.
+    """
+
+    def __init__(self, message: str, n_cuda: int = 0):
+        super().__init__(message)
+        self.n_cuda = n_cuda
+
+
+# IDEA: Make the config an observer, so that when the dtype or device is changed, all the tensors
+#   are changed. This can be done by adding a list of observers and calling a method when the
+#   configuration is changed.
 class Config(metaclass=_SingletonMeta):
     """
-    Configuration class for the library. This class is a singleton, so it is shared across the whole library.
+    Configuration class for the library. This class is a singleton, so it is shared across the
+    whole library.
     """
     # The dtype selected by default
     dtype = torch.float64
@@ -45,6 +60,12 @@ class Config(metaclass=_SingletonMeta):
 
     @property
     def eps(self):
+        """
+        Get the positive minimum for kernel parameters. This is usually slightly larger than zero
+        to avoid numerical errors.
+
+        :return:
+        """
         if self._eps is None:
             self._eps = torch.tensor(torch.finfo(self.dtype).eps)
         return self._eps
@@ -53,82 +74,150 @@ class Config(metaclass=_SingletonMeta):
     def eps(self, value):
         self._eps = torch.tensor(value)
 
+    @classmethod
+    def use_half_precision(cls):
+        """
+        Use half precision (float16) for all tensors. This may be much faster on GPUs,
+        but has reduced precision and may more often cause numerical instability. Only
+        recommended on GPUs.
+        """
+        if cls.device.type == 'cpu':
+            msg = "WARNING: half precision not recommend on CPU"
+            _log.warning(msg)
+            warnings.warn(msg, UserWarning, stacklevel=2)
+        cls.dtype = torch.float16
+
+    @classmethod
+    def use_single_precision(cls):
+        """
+        Use single precision (float32) for all tensors. This may be faster on GPUs,
+        but has reduced precision and may more often cause numerical instability.
+        """
+        cls.dtype = torch.float32
+
+    @classmethod
+    def use_double_precision(cls):
+        """
+        Use double precision (float64) for all tensors. This is the recommended precision for
+        numerical stability, but can be significantly slower.
+        """
+        cls.dtype = torch.float64
+
+    @classmethod
+    def use_cpu(cls, n=None):
+        """
+        Use the CPU instead of the GPU for tensor calculations. This is the default if no GPU is
+        available. If you have more than one CPU, you can use a specific CPU by setting `n`.
+        """
+        if n is None:
+            cls.device = torch.device('cpu')
+        else:
+            cls.device = torch.device('cpu', n)
+
+    @classmethod
+    def use_gpu(cls, n: t.Optional[int] = None):
+        """
+        Use the GPU instead of the CPU for tensor calculations. This is the default if a GPU is
+        available. If you have more than one GPU, you can use a specific GPU by setting `n`.
+        """
+        if not torch.cuda.is_available():
+            msg = "CUDA is not available"
+            _log.error(msg)
+            raise CudaNotAvailableError(msg)
+
+        if n is not None:
+            if not isinstance(n, int):
+                msg = "The GPU number must be an integer"
+                _log.exception(msg)
+                raise TypeError(msg)
+            if n < 0:
+                msg = "The GPU number must be a non-negative integer"
+                _log.exception(msg)
+                raise ValueError(msg)
+            if torch.cuda.device_count() <= n:
+                msg = f"CUDA GPU '{n}' is not available"
+                _log.error(msg)
+                raise CudaNotAvailableError(msg, n)
+
+            cls.device = torch.device('cuda', n)
+
+        cls.device = torch.device('cuda', torch.cuda.current_device())
+
+    @classmethod
+    def print_gpu_information(cls):
+        """
+        Print information about whether CUDA is supported, and if so which GPU is being used.
+        """
+        if not torch.cuda.is_available():
+            msg = "CUDA is not available"
+            print(msg)
+            return
+
+        print("CUDA is available:")
+        current = None
+        if cls.device.type == 'cuda':
+            current = cls.device.index
+        for n in range(torch.cuda.device_count()):
+            print(
+                "%2d  %s%s" % (
+                    n, torch.cuda.get_device_name(n), " (selected)" if n == current else "")
+            )
+
+    @classmethod
+    def set_eps(cls, val):
+        """
+        Set the positive minimum for kernel parameters. This is usually slightly larger than zero
+        to avoid numerical instabilities.
+        """
+        cls.eps = val
+
+    def __repr__(self):
+        return f"Config(dtype={self.dtype}, device={self.device}, eps={self.eps:.2e})"
+
 
 config = Config()
 conf = config  # Alias for config
 
 
+# noinspection PyMissingOrEmptyDocstring
 def use_half_precision():
-    """
-    Use half precision (float16) for all tensors. This may be much faster on GPUs, but has reduced precision and may
-    more often cause numerical instability. Only recommended on GPUs.
-    """
-    if config.device.type == 'cpu':
-        print('WARNING: half precision not recommend on CPU')
-    config.dtype = torch.float16
+    conf.use_half_precision()
 
 
+# noinspection PyMissingOrEmptyDocstring
 def use_single_precision():
-    """
-    Use single precision (float32) for all tensors. This may be faster on GPUs, but has reduced precision and may more
-    often cause numerical instability.
-    """
-    config.dtype = torch.float32
+    conf.use_single_precision()
 
 
+# noinspection PyMissingOrEmptyDocstring
 def use_double_precision():
-    """
-    Use double precision (float64) for all tensors. This is the recommended precision for numerical stability,
-    but can be significantly slower.
-    """
-    config.dtype = torch.float64
+    conf.use_double_precision()
 
 
+# noinspection PyMissingOrEmptyDocstring
 def use_cpu(n=None):
-    """
-    Use the CPU instead of the GPU for tensor calculations. This is the default if no GPU is available.
-    If you have more than one CPU, you can use a specific CPU by setting `n`.
-    """
-    if n is None:
-        config.device = torch.device('cpu')
-    else:
-        config.device = torch.device('cpu', n)
+    conf.use_cpu(n)
 
 
+# noinspection PyMissingOrEmptyDocstring
 def use_gpu(n=None):
-    """
-    Use the GPU instead of the CPU for tensor calculations. This is the default if a GPU is available.
-    If you have more than one GPU, you can use a specific GPU by setting `n`.
-    """
-    if not torch.cuda.is_available():
-        _log.error("CUDA is not available")
-    elif n is not None and (not isinstance(n, int) or n < 0 or torch.cuda.device_count() <= n):
-        _log.error("CUDA GPU '%s' is not available" % (n,))
-    elif n is None:
-        config.device = torch.device('cuda', torch.cuda.current_device())
-    else:
-        config.device = torch.device('cuda', n)
+    conf.use_gpu(n)
 
 
+# noinspection PyMissingOrEmptyDocstring
 def print_gpu_information():
-    """
-    Print information about whether CUDA is supported, and if so which GPU is being used.
-    """
-    if not torch.cuda.is_available():
-        print("CUDA is not available")
-        return
-
-    print("CUDA is available:")
-    current = None
-    if config.device.type == 'cuda':
-        current = config.device.index
-    for n in range(torch.cuda.device_count()):
-        print("%2d  %s%s" % (n, torch.cuda.get_device_name(n), " (selected)" if n == current else ""))
+    conf.print_gpu_information()
 
 
+# noinspection PyMissingOrEmptyDocstring
 def set_eps(val):
-    """
-    Set the positive minimum for kernel parameters. This is usually slightly larger than zero to avoid numerical
-    instabilities. Default is at 1e-8.
-    """
-    config.eps = val
+    conf.set_eps(val)
+
+
+use_half_precision.__doc__ = Config.use_half_precision.__doc__
+use_single_precision.__doc__ = Config.use_single_precision.__doc__
+use_double_precision.__doc__ = Config.use_double_precision.__doc__
+use_cpu.__doc__ = Config.use_cpu.__doc__
+use_gpu.__doc__ = Config.use_gpu.__doc__
+print_gpu_information.__doc__ = Config.print_gpu_information.__doc__
+set_eps.__doc__ = Config.set_eps.__doc__
